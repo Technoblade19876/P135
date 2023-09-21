da26691a98d7e063208d3a52259982bdee9c2c99
Gandham Dheeraj <121343901+Technoblade19876@users.noreply.github.com>
2023-09-21 16:29:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
+noseX = 0;
+noseY = 0;
+
+leftwristX = 0;
+rightwristX = 0;
+
+size = 0;
+
 function setup() {
-    canvas = createCanvas(300, 300);
+    canvas = createCanvas(800, 600);
     video = createCapture(VIDEO);
-    video.size(300, 300);
-    canvas.position(450, 250);
+    video.size(420, 420);
+    canvas.position(600, 250);
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose',gotPoses);
     }
     
     function draw(){
         background("#00ffff");
+
+        text("This is the text" , noseX, noseY);
+        textSize(size)
+
+        size = leftwristX - rightwristX;
+        size = Math.floor(size);
+        console.log(size);
     }
     
     function modelLoaded() {
@@ -19,5 +34,12 @@ function setup() {
     function gotPoses(results){
         if(results.length > 0){
             console.log(results);
+
+            noseX = results[0].pose.nose.x;
+            noseY = results[0].pose.nose.y;
+    
+            rightwristX = results[0].pose.rightWrist.x;
+            leftwristX = results[0].pose.leftWrist.x;
         }
+
     }
